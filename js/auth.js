77e62c5cd75f584e1faa3f423b1271c1608a27eb
mvillen647-crypto <mvillen647@gmail.com>
2026-07-09 14:37:40 +0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,10 +1,18 @@
 import { supabase } from "./supabase.js";
 
 
+
+// REGISTER
+
 window.register = async function(){
+
 
 const name =
 document.getElementById("name").value;
+
+
+const phone =
+document.getElementById("phone").value;
 
 
 const email =
@@ -21,32 +29,111 @@ document.getElementById("role").value;
 
 
 const {data,error} =
+
 await supabase.auth.signUp({
 
 email,
-password,
+password
 
-options:{
-data:{
-name,
-role
-}
-}
 
 });
+
 
 
 if(error){
 
 alert(error.message);
+return;
 
-}else{
+}
+
+
+
+
+const user = data.user;
+
+
+
+const {error:profileError}=
+
+await supabase
+
+.from("profiles")
+
+.insert({
+
+id:user.id,
+
+name,
+
+phone,
+
+role
+
+});
+
+
+
+
+
+if(profileError){
+
+alert(profileError.message);
+
+return;
+
+}
+
+
 
 alert("Account created!");
 
-window.location.href="login.html";
+window.location.href="index.html";
+
+
+};
+
+
+
+
+
+
+
+// LOGIN
+
+window.login = async function(){
+
+
+const email =
+document.getElementById("email").value;
+
+
+const password =
+document.getElementById("password").value;
+
+
+
+const {error}=
+
+await supabase.auth.signInWithPassword({
+
+email,
+password
+
+});
+
+
+
+if(error){
+
+alert(error.message);
+return;
 
 }
 
 
-}
+
+window.location.href="index.html";
+
+
+};
